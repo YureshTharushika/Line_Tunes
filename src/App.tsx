cb3869900858829a0ef1de1d01369c1541a3e92d
Yuresh Tharushika <yuresh.official@gmail.com>
2024-11-09 20:46:38 +0530

    changed sequencre cirle to a lane

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import AudioCD from './AudioCD';
+import AudioSequencer from './AudioSequencer';
 
 function App() {
   
   return (
     <div className="min-h-screen bg-gray-50 p-4">
       <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold text-gray-900">Atomic Tunes</h1>
+        <h1 className="text-3xl font-bold text-gray-900">Line Tunes</h1>
       </header>
       <main>
-        <AudioCD />
+        <AudioSequencer />
       </main>
     </div>
   );
